cbeefe72a3e2d88a2f2be7af01c8bf1aeaaf416b
Megan Dunbar <meganleedunbar@gmail.com>
2015-07-28 11:40:38 -0400

    added page styles, links for contacts

---
+++ scripts/views/contact.js
@@ -0,0 +1,31 @@
+import HeaderView from './header';
+
+export default Backbone.View.extend({
+
+  template: JST['contact'],
+
+  events: {
+    'click .index-link .contact-link .resume-link .github-link .experience-link' : 'remove'
+  },
+
+  initialize: function(){
+    this.render();
+    this.headerView = new HeaderView();
+  },
+
+  render: function(options) {
+    this.$el.prepend(this.headerView);
+    this.$el.append(this.template);
+  },
+
+  //remove not functional
+    remove: function() {
+      // COMPLETELY UNBIND THE VIEW
+      this.undelegateEvents();
+      this.$el.removeData().unbind();
+      // Remove view from DOM
+      this.remove();
+      Backbone.View.prototype.remove.call(this);
+    }
+
+});
