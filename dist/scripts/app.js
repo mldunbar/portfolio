415506d009964aa95886936a62aa9ed2c5057940
Megan Dunbar <meganleedunbar@gmail.com>
2015-07-27 20:49:50 -0400

    prepare to launch

---
+++ dist/scripts/app.js
@@ -0,0 +1,14 @@
+require.register("main", function(exports, require, module){
+  'use strict';
+
+(function () {
+  'use strict';
+
+  $(document).ready(function () {
+    $('body').prepend(JST.application());
+  });
+})();
+  
+});
+
+//# sourceMappingURL=app.js.map
